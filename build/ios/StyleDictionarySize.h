02efba773913e9ca36e7280963fa9b76e7fdfa17
Masashi Ogawa <masashi.ogawa@lubricate.dev>
2022-07-14 19:01:55 +0900

    init style-dictionary

---
+++ build/ios/StyleDictionarySize.h
@@ -0,0 +1,15 @@
+
+// StyleDictionarySize.h
+//
+
+// Do not edit directly
+// Generated on Thu, 14 Jul 2022 10:01:50 GMT
+
+
+#import <Foundation/Foundation.h>
+
+
+extern float const SizeFontSmall;
+extern float const SizeFontMedium;
+extern float const SizeFontLarge;
+extern float const SizeFontBase;
